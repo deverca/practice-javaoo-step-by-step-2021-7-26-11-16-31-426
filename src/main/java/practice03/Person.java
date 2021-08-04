e338dd7540664ebe60ba8beb9674ed99d8e71407
deverca <deverca@oocl.com>
2021-08-04 16:15:03 +0800

    [OOP][CARMELA] feat: should_student_have_name_age_and_class_number in practice3

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -4,6 +4,13 @@ public class Person {
     private String name;
     private int age;
 
+
+    public String introduce() {
+
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
