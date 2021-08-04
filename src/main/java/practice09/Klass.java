7f4309f91dd50324f210ed6bbdaedd7575f6a866
deverca <deverca@oocl.com>
2021-08-04 21:22:56 +0800

    [OOP][CARMELA]feat: should_class_have_a_number in practice9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,17 @@
 package practice09;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
