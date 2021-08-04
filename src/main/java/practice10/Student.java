f65e0c80539ded528090536461ddd315f1520356
deverca <deverca@oocl.com>
2021-08-04 22:46:05 +0800

    [OOP][CARMELA]feat:should_teacher_have_name_and_age_and_classes in practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -15,13 +15,17 @@ public class Student extends Person {
 
         return klass;
     }
-        public String introduce(){
-            String introduceString = MessageFormat.format("{0} I am a Student. ", super.introduce());
-            if (!this.equals(klass.getLeader())) {
-                introduceString += MessageFormat.format("I am at {0}.", klass.getDisplayName());
 
-            }
-            return introduceString;
+    public String introduce() {
+        String introduceString = MessageFormat.format("{0} I am a Student. ", super.introduce());
+        if (!this.equals(klass.getLeader())) {
+            introduceString += MessageFormat.format("I am at {0}.", klass.getDisplayName());
+
+        } else {
+
+            introduceString += MessageFormat.format("I am Leader of {0}.", klass.getDisplayName());
         }
+        return introduceString;
+    }
 
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,32 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private Klass klass;
+    private LinkedList<Klass> linkedList;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.klass = klass;
+        this.linkedList = linkedList;
+
+    }
+
+
+    public LinkedList<Klass> getClasses() {
+        return linkedList;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
