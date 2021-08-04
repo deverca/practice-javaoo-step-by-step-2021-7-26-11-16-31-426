3d75b747747113b89e6d38f0e2ccd377bc04ce2d
deverca <deverca@oocl.com>
2021-08-04 19:39:15 +0800

    [OOP][CARMELA]feat:should_student_have_name_age_and_class_number in practice7

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -5,6 +5,14 @@ package practice07;
 public class Student extends Person {
     private Klass klass;
 
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
