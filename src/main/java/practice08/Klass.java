6e2b5879509a512adf8cf91edf94a8677112c113
deverca <deverca@oocl.com>
2021-08-04 20:45:58 +0800

    [OOP][CARMELA]feat:should_class_get_display_name in practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -15,4 +15,7 @@ public class Klass {
     public void setNumber(int number) {
         this.number = number;
     }
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
