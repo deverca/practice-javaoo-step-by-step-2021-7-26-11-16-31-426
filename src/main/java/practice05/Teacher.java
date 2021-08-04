3c20b1f4f37b5b0c7b7063d4b63f1a38f172917f
deverca <deverca@oocl.com>
2021-08-04 17:41:57 +0800

    [OOP][CARMELA] feat: should_teacher_have_name_and_age_and_class in practice5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,19 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 }
