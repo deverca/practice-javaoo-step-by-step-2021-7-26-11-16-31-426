c5fbc65faf04635e456d52a1e99cbcd8d23e15a9
deverca <deverca@oocl.com>
2021-08-04 16:26:42 +0800

    [OOP][CARMELA] feat: should_person_have_an_introduce_method_which_introduce_person_with_name_and_age in practice4

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -25,4 +25,10 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+
 }
