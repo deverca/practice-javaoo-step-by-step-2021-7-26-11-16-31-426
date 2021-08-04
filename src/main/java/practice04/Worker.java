328fb7d817e93862c88f955448f6768d22d74557
deverca <deverca@oocl.com>
2021-08-04 16:33:56 +0800

    [OOP][CARMELA] feat: should_worker_introduce_itself_with_no_field in practice4

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -4,4 +4,8 @@ public class Worker extends  Person{
     public Worker(String name, int age) {
         super(name, age);
     }
+    @Override
+    public String introduce(){
+        return super.introduce()+" I am a Worker. I have a job.";
+    }
 }
