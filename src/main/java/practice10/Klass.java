653f768b0ea96672b2a5a2a52eace99cb5ffe81a
deverca <deverca@oocl.com>
2021-08-04 22:40:31 +0800

    [OOP][CARMELA]feat:should_class_not_assign_a_student_as_leader_when_student_is_not_a_member in practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,8 +1,15 @@
 package practice10;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
 
     private int number;
+    private Student leader;
+    private List<Student> members;
+
 
     public int getNumber() {
         return number;
@@ -13,5 +20,25 @@ public class Klass {
     }
     public String getDisplayName() {
         return "Class " + number;
+    }
+    public void assignLeader(Student leader) {
+        if (members != null && members.contains(leader)) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+    public void appendMember(Student student) {
+        members = new ArrayList<>();
+        members.add(student);
+
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
     }
 }
