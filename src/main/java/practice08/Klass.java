35e86a33c34d4c920afc52bec1f3a8d061c4c8a2
deverca <deverca@oocl.com>
2021-08-04 21:03:36 +0800

    [OOP][CARMELA]feat:should_class_assign_a_member_student_as_leader in practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,9 +1,22 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
 
     private int number;
     private Student leader;
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Student> members) {
+        this.members = members;
+    }
+
+    private List<Student> members;
 
 
     public Klass(int number) {
@@ -21,17 +34,27 @@ public class Klass {
     public void setNumber(int number) {
         this.number = number;
     }
+
     public String getDisplayName() {
         return "Class " + number;
     }
-    public void assignLeader(Student leader) {
-       // this.leader = leader;
-       System.out.print("It is not one of us.\n");
 
+    public void assignLeader(Student leader) {
+        if (members != null && members.contains(leader)) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
     }
 
     public Student getLeader() {
         return leader;
+    }
+
+    public void appendMember(Student student) {
+        members = new ArrayList<>();
+        members.add(student);
+
     }
 
 }
