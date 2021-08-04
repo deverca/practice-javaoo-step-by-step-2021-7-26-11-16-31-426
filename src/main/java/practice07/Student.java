c964c13cdf19ce2bdd46400c3b2eee3eaa6c26b2
deverca <deverca@oocl.com>
2021-08-04 19:40:27 +0800

    [OOP][CARMELA]feat:should_student_introduce_with_class in practice7

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,6 +1,7 @@
 package practice07;
 
 
+import java.text.MessageFormat;
 
 public class Student extends Person {
     private Klass klass;
@@ -16,5 +17,9 @@ public class Student extends Person {
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
+    }
+    public String introduce() {
+        return MessageFormat.format(
+                "{0} I am a Student. I am at {1}.", super.introduce(), klass.getDisplayName());
     }
 }
