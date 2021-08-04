509f34cb35a3f6a8f772a83caa416c77239b7f71
deverca <deverca@oocl.com>
2021-08-04 22:43:03 +0800

    [OOP][CARMELA]feat:should_student_introduce_with_class in practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,6 +1,8 @@
 package practice10;
 
 
+import java.text.MessageFormat;
+
 public class Student extends Person {
     private Klass klass;
 
@@ -10,6 +12,16 @@ public class Student extends Person {
     }
 
     public Klass getKlass() {
+
         return klass;
     }
+        public String introduce(){
+            String introduceString = MessageFormat.format("{0} I am a Student. ", super.introduce());
+            if (!this.equals(klass.getLeader())) {
+                introduceString += MessageFormat.format("I am at {0}.", klass.getDisplayName());
+
+            }
+            return introduceString;
+        }
+
 }
