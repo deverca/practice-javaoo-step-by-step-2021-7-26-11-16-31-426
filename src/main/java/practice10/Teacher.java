6526f7f5c809b011bd9f34622e734932a0a03305
deverca <deverca@oocl.com>
2021-08-04 22:50:53 +0800

    [OOP][CARMELA]feat:should_teacher_introduce_a_student_it_teaches in practice10

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,7 +1,10 @@
 package practice10;
 
 
+import java.text.MessageFormat;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Teacher extends Person {
     private Klass klass;
@@ -42,6 +45,18 @@ public class Teacher extends Person {
         }
 
         return introduce.toString();
+    }
+    public String introduceWith(Student student) {
+
+        List<Klass> newLinked = new LinkedList();
+        String introduceString = MessageFormat.format("{0} I am a Teacher. I ", super.introduce());
+        newLinked =linkedList.stream().filter(klass->klass.getNumber()==student.getKlass().getNumber()).collect(Collectors.toList());
+
+        if(!newLinked.isEmpty()){
+            introduceString += MessageFormat.format("teach {0}.", student.getName());
+        }
+        return introduceString;
+
     }
     public boolean isTeaching(Student student) {
         return linkedList.contains(student.getKlass());
