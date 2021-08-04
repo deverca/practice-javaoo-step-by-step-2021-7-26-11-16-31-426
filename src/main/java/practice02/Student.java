e1adac24e6dcf7703141696e81e61198da8b2a45
deverca <deverca@oocl.com>
2021-08-04 16:10:17 +0800

    [OOP][CARMELA] feat:should_student_introduce_with_class

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,6 +1,6 @@
 package practice02;
 
-public class Student extends Person{
+public class Student extends Person {
 
     public int getKlass() {
         return klass;
@@ -11,8 +11,16 @@ public class Student extends Person{
     }
 
     private int klass;
+
     public Student(String name, int age, int klass) {
         super(name, age);
         this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        return "I am a Student. I am at Class " + klass + ".";
+
     }
 }
