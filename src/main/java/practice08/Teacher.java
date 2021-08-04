cb6caef2e204dc80eeda903b43ed1c466150b183
deverca <deverca@oocl.com>
2021-08-04 21:16:11 +0800

    [OOP][CARMELA]feat:should_teacher_introduce_itself_with_which_class_it_teaches in practice8

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -6,7 +6,9 @@ import java.text.MessageFormat;
 
 public class Teacher extends Person{
     private Klass klass;
-
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass=klass;
@@ -24,10 +26,9 @@ public class Teacher extends Person{
 
        if (klass!= null) {
            introduce += "I teach " + klass.getDisplayName() + ".";
+       } else {
+           introduce += "I teach No Class.";
        }
-//       } else {
-//           introduce += "I teach No Class.";
-//       }
 
        return introduce;
    }
